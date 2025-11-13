c4a3625ac668e6500876f4da35884d5caa844c08
mirateodora <ashadora154@gmail.com>
2025-11-13 13:55:11 +0200

    add color extraction logic

---
+++ palette_utils.py
@@ -0,0 +1,27 @@
+import cv2
+import numpy as np
+from sklearn.cluster import KMeans
+
+def extract_colors(img_path, n):
+    img = cv2.imread(img_path)
+    if img is None:
+        raise FileNotFoundError("Image not found")
+
+    img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
+    img = cv2.resize(img, (600, 400))
+
+    pixels = img.reshape(-1, 3)
+
+    kmeans = KMeans(n_clusters=n, random_state=42)
+    kmeans.fit(pixels)
+    colors = kmeans.cluster_centers_.astype(int)
+
+    labels, counts = np.unique(colors, return_counts=True)
+    sorted_idx = np.argsort(-counts)
+    sorted_colors = colors[sorted_idx]
+
+    return sorted_colors
+
+
+def rgb_to_hex(rgb):
+    return '#{:02x}{:02x}{:02x}'.format(*rgb)
